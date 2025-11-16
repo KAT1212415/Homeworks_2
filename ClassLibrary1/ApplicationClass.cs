923622afbb2597b95f6c6bd749ba0b45ed728c77
malin <e2024024@gmail.com>
2025-11-16 13:14:44 +0300

    ДЗ 3.3 Введение в RevitAPI

---
+++ ClassLibrary1/ApplicationClass.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.Attributes;
+
+namespace ClassLibrary1
+{
+
+    [Transaction(TransactionMode.Manual)]
+    public class ApplicationClass : IExternalApplication
+    {
+        public Result OnStartup(UIControlledApplication application)
+        {
+            application.CreateRibbonTab("ПИК-Привет");
+            var panel = application.CreateRibbonPanel("ПИК-Привет", "Общее");
+            var button = new PushButtonData(
+                "Hello",
+                "Привет",
+                "C:\\Software Development Kit\\Samples\\DuplicateViews\\CS\\bin\\Debug\\DuplicateViews.dll",
+                "Revit.SDK.Samples.DuplicateViews.CS.DuplicateAcrossDocumentsCommand"
+                );
+            //BitmapImage bitmapImage = new BitmapImage(new Uri("C:\\Users\\shvetses\\AppData\\Roaming\\Autodesk\\Revit\\Addins\\2019\\Test\\Img\\BDSBuildingPartWriter32.png", UriKind.Absolute));
+            //button.LargeImage = bitmapImage;
+            panel.AddItem(button);
+            return Result.Succeeded;
+        }
+
+        public Result OnShutdown(UIControlledApplication application)
+        {
+            return Result.Succeeded;
+        }
+    }
+}
