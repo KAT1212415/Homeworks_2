6bbdf11ec8f9c97112659e2b9b99b792b761bb9f
malin <e2024024@gmail.com>
2025-11-23 05:03:04 +0300

    3.5 Выбор элементов

---
+++ ClassLibrary3/Class1.cs
@@ -0,0 +1,89 @@
+﻿using Autodesk.Revit.ApplicationServices;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.DB.Mechanical;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+using System.Collections.Generic;
+
+
+namespace ClassLibrary3
+{
+    [Transaction(TransactionMode.Manual)]
+    public class CommandClass : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+
+            UIApplication uiApp = commandData.Application;
+            Application app = uiApp.Application;
+            UIDocument uiDoc = uiApp.ActiveUIDocument;
+            Document doc = uiDoc.Document;
+
+            try
+            {
+
+                IList<Reference> pickedRefs = uiDoc.Selection.PickObjects(ObjectType.Element, new FamilyInstanceFilter(), "Выберите элементы");
+                Dictionary<string, int> FamilyInstances = new Dictionary<string, int>();
+
+                foreach (Reference reference in pickedRefs)
+                {
+                    
+                    Element elem = doc.GetElement(reference);
+                    //FamilyInstance familyInstance = elem as FamilyInstance;
+
+                   
+                        string CategoryType = ((BuiltInCategory)elem.Category.Id.IntegerValue).ToString();
+
+                        //FamilyInstance familyInstance = elem as FamilyInstance;
+                        //Category Category = familyInstance.Category;
+                        //BuiltIncategory BuiltIncategory=
+                       
+
+
+                        bool newCategoryType = FamilyInstances.ContainsKey(CategoryType);
+                        if (newCategoryType == true)
+                        {
+                            
+                            int n = FamilyInstances[CategoryType];
+                            n = n + 1;
+                            FamilyInstances[CategoryType] = n;
+
+                        }
+                        else
+                        {
+                            FamilyInstances.Add(CategoryType, 1);
+                        }
+                    
+                }
+
+                string allItems = "";
+                foreach (var item in FamilyInstances)
+                {
+                    allItems += $"{item.Key}: {item.Value}\n";
+                }
+                TaskDialog.Show("Все элементы", allItems);
+
+
+            }
+            catch
+            {
+                TaskDialog.Show("Инфо", $"Элементы не выбраны");
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+            return Result.Succeeded;
+        }
+    }
+
+}
