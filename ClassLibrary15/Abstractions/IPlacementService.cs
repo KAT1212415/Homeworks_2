ca985e5ff7db1112320a93e851fcd65f90a5b91c
malin <e2024024@gmail.com>
2026-01-29 07:56:33 +0300

    3.10 Плагин "Размещение мебели"

---
+++ ClassLibrary15/Abstractions/IPlacementService.cs
@@ -0,0 +1,11 @@
+﻿
+﻿using CSharpFunctionalExtensions;
+using ClassLibrary15.Models;
+
+namespace ClassLibrary15.Abstractions
+{
+    public interface IPlacementService
+    {
+        Result Place(FurnitureType selectedFurnitureType, int count);
+    }
+}
---
+++ ClassLibrary15/Views/MainWindow.xaml.cs
@@ -0,0 +1,18 @@
+﻿
+using ClassLibrary15.ViewModels;
+using System.Windows;
+
+namespace ClassLibrary15.Views
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow(MainWindowViewModel mainWindowViewModel)
+        {
+            DataContext = mainWindowViewModel;
+            InitializeComponent();
+        }
+    }
+}
